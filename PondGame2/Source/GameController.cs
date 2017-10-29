930eb3e617d55225783b273a9b01cc3a6e52440a
Anthony Robertson <anthonyrobertson050@gmail.com>
2017-10-29 12:14:04 -0400

    Inital Commit
    skeleton of .net game

---
+++ PondGame2/Source/GameController.cs
@@ -0,0 +1,27 @@
+﻿using Encog.Neural.Networks;
+using KinectProject.Source.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectProject.Source
+{
+    class GameController
+    {
+        public GameController()
+        {
+        }
+
+        public void update()
+        {
+        }
+
+        public void draw(Render render)
+        {
+
+        }
+
+    }
+}
